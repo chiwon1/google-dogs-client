2de67fd33c097e62783a5d4760917e13e68139bc
Chiwon <emphasize1@gmail.com>
2021-08-24 10:21:32 +0900

    Feat: set socket.io
     - components/TextEditor

--- src/components/TextEditor/TextEditor.js
+++ src/components/TextEditor/TextEditor.js
@@ -1,7 +1,11 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import "./styles.css";
+import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
+
+const SAVE_INTERVAL = 2000;
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -13,29 +17,102 @@ const TOOLBAR_OPTIONS = [
   [{ align: [] }],
   ["image", "blockquote", "code-block"],
   ["clean"],
-]
+];
 
-function TextEditor() {
-  // TODO : 작동원리 파악
-  const wrapperRef = useCallback((wrapper) => {
-    if (wrapper === null) {
+export default function TextEditor() {
+  const { id: documentId } = useParams();
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+
+    return () => {
+      s.disconnect();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (socket == null || quill == null) {
+      return;
+    }
+
+    socket.once("load-document", document => {
+      quill.setContents(document);
+      quill.enable();
+    })
+
+    socket.emit("get-document", documentId);
+  }, [socket, quill, documentId])
+
+  useEffect(() => {
+    if (socket == null || quill == null) {
+      return;
+    }
+
+    const interval = setInterval(() => {
+      socket.emit("save-document", quill.getContents())
+    }, SAVE_INTERVAL);
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) {
+      return;
+    }
+
+    const handler = delta => {
+      quill.updateContents(delta)
+    };
+
+    socket.on("receive-changes", handler);
+
+    return () => {
+      socket.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
+
+  useEffect(() => {
+    if (socket == null || quill == null) {
+      return;
+    }
+
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") {
+        return;
+      }
+
+      socket.emit("send-changes", delta);
+    };
+    quill.on("text-change", handler);
+
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
+
+  const wrapperRef = useCallback(wrapper => {
+    if (wrapper == null) {
       return;
     }
 
     wrapper.innerHTML = "";
-
     const editor = document.createElement("div");
-
     wrapper.append(editor);
-
-    new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS } });
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    q.disable();
+    q.setText("Loading...");
+    setQuill(q);
   }, []);
 
   return (
-    <div>
-      <div className="container" ref={wrapperRef}></div>
-    </div>
+    <div className="container" ref={wrapperRef}></div>
   );
 }
-
-export default TextEditor;
